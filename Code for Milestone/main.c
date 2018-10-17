8a0a0a0e037685a703e193ba5f4a5f5ea2c73979
DJ Stahlberger <stahlberd8@students.rowan.edu>
2018-10-17 18:56:40 -0400

    IT F***ING WORKS
    Spent several hours trying to "fix" my code just to eventually find out my board is broken. How, I don't know...

--- Code for Milestone/main.c
+++ Code for Milestone/main.c
@@ -13,18 +13,23 @@ void PinSetup(void){
 
     //P1.6 -- Red -- TA0CCR0
     P1SEL |= BIT6;
-    P1SEL &= ~BIT6;             //Connects P1.6 to TimerA
+    P1SEL2 &= ~BIT6;             //Connects P1.6 to TimerA
     P1DIR |= BIT6;              //Sets output direction
 
     //P2.1 -- Green -- TA1CCR1
     P2SEL |= BIT1;
-    P2SEL &= ~BIT1;             //Connects P2.1 to TimerA
+    P2SEL2 &= ~BIT1;             //Connects P2.1 to TimerA
     P2DIR |= BIT1;              //Sets output direction
 
     //P2.4 -- Blue -- TA1CCR2
     P2SEL |= BIT4;
-    P2SEL &= ~BIT4;             //Connects P2.4 to TimerA
+    P2SEL2 &= ~BIT4;             //Connects P2.4 to TimerA
     P2DIR |= BIT4;              //Sets output direction
+
+
+    P1DIR |= BIT0;
+    P1OUT &= ~BIT0;
+
 }
 
 void timerASetup(void){
@@ -62,6 +67,8 @@ void UARTsetup(void){
 
     UCA0CTL1 &= ~UCSWRST;                        //Disables UART reset
     UC0IE |= UCA0RXIE;                           //Enables UART interrupt
+
+    //UC0IE |= UCA0TXIE;
 }
 
 void main(void){
@@ -72,20 +79,24 @@ void main(void){
     timerASetup();
     UARTsetup();
 
-    __bis_SR_register(LPM0_bits + GIE);         //global interrupts enabled + low power mode
+    __bis_SR_register(GIE);         //global interrupts enabled + low power mode
 }
 
 
 
-//TA0CCR0 = RED
+//TA0CCR1 = RED
 //TA1CCR1 = GREEN
 //TA1CCR2 = BLUE
 //UCA0RXBUF DATA RECIEVED
 //UCA0TXBUF DATA TRANSMITTED
-#pragma vector = USCIABORX_VECTOR;              //Setting the interrupt condition for the button press
+#pragma vector = USCIAB0RX_VECTOR;              //Setting the interrupt condition for the button press
 __interrupt void usciaborx (void){
 
+    P1OUT |= BIT0;
+
     char data = UCA0RXBUF;                      //stores byte from UART into data char
+
+    UC0IE |= UCA0TXIE;
 
     if(count == 0){                             //Initial if statement designed to run at the first package byte
         count = data;                           //count gets the number of bytes in the package
@@ -93,31 +104,50 @@ __interrupt void usciaborx (void){
         total = data;                           //total also gets number of bytes in the package
 
 
-        if(data >=8 | (data-2)%3 == 0)          //More than 8 bytes left or there are enough
+        if(data >=8){ //| (data-2)%3 == 0)          //More than 8 bytes left or there are enough
                                                 //bytes for another node to use
             UCA0TXBUF = data - 3;               //package size gets sent to the next node
+        }
     }
-    else if(total - count = 1)                  //if the byte being read is directly after the size byte
-        TA0CCR0 = data;                         //set the value for the red
-    else if(total - count = 2)                  //if the byte being read is 2 after the size byte
+
+    else if(total - count == 1){                 //if the byte being read is directly after the size byte
+        TA0CCR1 = data;                         //set the value for the red
+    }
+
+    else if(total - count == 2){                  //if the byte being read is 2 after the size byte
         TA1CCR1 = data;                         //set the value for the green
-    else if(total - count = 3)                  //if the byte being read is 3 after the size byte
+    }
+
+    else if(total - count == 3){                  //if the byte being read is 3 after the size byte
         TA1CCR2 = data;                         //sets the value for the blue
+    }
+
     else{
-        if(data >=8 | (data-2)%3 == 0)          //more than 8 bytes left or there are enough
+        if(total >=8){// | (data-2)%3 == 0){          //more than 8 bytes left or there are enough
                                                 //bytes for another node to use
             UCA0TXBUF = data;                   //transmit the byte to the next node
+        }
     }
 
-    count--;                                    //incrementally counts down from the total package size until 0
+    count --;                                    //incrementally counts down from the total package size until 0
 
-   }
+    P1OUT &= ~BIT0;
 
-
-
-
+}
 
 
+
+
+
+
+#pragma vector = USCIAB0TX_VECTOR;
+__interrupt void usciab0tx (void){
+
+    P1OUT |= BIT0;
+
+    UC0IE &= ~UCA0TXIE;
+    P1OUT &= ~BIT0;
+}
 
 
 
